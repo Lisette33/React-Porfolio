7d35a799ac50d4c4cf5c3ce4687b74bda85b0e53
Lisette33 <113862182+Lisette33@users.noreply.github.com>
2023-03-13 21:45:18 -0400

    Updated portfolio

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -11,9 +11,19 @@ export default function About() {
     {/* <h3>{navTitleFour}</h3> */}
     <ul class="project-list"> 
         <ul class="card4">
-          <b><p>My interest is full-stack web development.</p></b>
-          <br />
-          <b><li>
+          <b><p>Skills:</p></b>
+          {/* <br /> */}
+          <p>
+             {/* <p class="tab"><b>Full-Stack Web Development Skills:</b></p> */}
+           
+             <p class="tabOne">HTML; CSS; JavaScript; Git; Bootstrap; The DOM; APIs; JQuery; JSON; AJAX </p>
+                {/* <br/> */}
+            <p class="tabTwo">Node; ES6; Object-oriented programming; Express; MySQL; MVC paradigm; 
+                Sequelize; Testing; Agile development</p>
+                {/* <br/> */}
+            <p class="tabThree">Progressive Web Apps; React; NoSQL; MERN Stack; Computer Science Fundamentals</p>
+    </p>
+          {/* <b><li>
             HTML
           </li></b>
           <br />
@@ -25,7 +35,7 @@ export default function About() {
             JavaScript
           </li></b>
           <br />
-          <br />
+          <br /> */}
             <a href="#">Resume</a>
         </ul>
     </ul>
