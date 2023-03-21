3453eb3d83fdfbbb0817cc47018a0f5e7a5417ac
Lisette33 <113862182+Lisette33@users.noreply.github.com>
2023-03-21 13:55:59 -0400

    Updated css

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -6,14 +6,17 @@ function Homepage({setCurrentPage}) {
   return (
     <div>
         {/* <div class="background"></div> */}
-        <div>
+        <div class="container">
+        <div style={{display:"flex",flex:"row", justifyContent:"center", padding:"0%", alignItems:"center"}}>
+        <div class="background-text text-center">
             <p>
                 <h1><b>I am Lisette Morales.
                 <br/>
                 Full-Stack Web Development</b></h1>
             </p>
         </div>
-       
+        </div>
+        </div>
 
     </div>
 
